4d91e081b4b83cb07468ad186e69cc5d90cf58ba
Usuario <Usuario>
2017-03-06 22:05:02 -0500

    casa falta quitar prompt

--- assets/js/js2.js
+++ assets/js/js2.js
@@ -1,25 +1,36 @@
 //Aqui va el codigo de la pagina de dropdown
+//data inicial
+var coders = [ { nombre : "Meche", apellido : "Zubieta",  promocion : "1era. promoción", estado : "Trabajando" },
+
+			 { nombre : "Arabela", apellido : "Rojas", promocion : "2da. promoción", estado : "Trabajando" },
+
+			 { nombre : "Maria", apellido : "Rosán",  promocion : "2da. promoción", estado : "Trabajando" },
+
+			 { nombre : "Guadalupe", apellido : "Racio",  promocion : "3era. promoción", estado : "Trabajando" },
+
+			 { nombre : "Sara", apellido : "Casa",  promocion : "3era. promoción", estado : "Trabajando" },
+
+			 { nombre : "Daniella", apellido : "Durán",  promocion : "4ta. promoción", estado : "Trabajando" },
+
+			 { nombre : "Michelle", apellido : "Seguil",  promocion : "5ta. promoción", estado : "Trabajando" },
+
+			 { nombre : "Diana", apellido : "Navarro",  promocion : "5ta. promoción", estado : "Trabajando" } ];
 
 var contenedor = document.getElementById("contenedor")
-var arrayEstudiantes=[];
-//este array tiene dentro objetos
-function Estudiante(nombre,apellido, promocion, estado){
-  this.name = nombre;
-  this.apellido = apellido;
-  this.promocion = promocion;
-  this.estado= estado;
-  this.print = function(){
-    return "Nombre: " + this.name + "<br>" +
-           "Apellido: " + this.apellido + "<br>" +
-           "Promocion: " + this.promocion + "<br>" +
-           "Estado: " + this.estado + "<br>";
-  }
+
+function print(item){
+
+  return     "<div class = 'div-coder'>" +
+              "Nombre: " + item.nombre + "<br>" +
+              "Apellido: " + item.apellido + "<br>" +
+              "Promocion: " + item.promocion + "<br>" +
+              "Estado: " + item.estado +
+              "</div>";
+
 }
 var frase = "";
-for(i=0; i<arrayEstudiantes.length;i++){
-
-  frase += arrayEstudiantes[i].print;
-
+for(i=0; i<coders.length;i++){
+  frase += print(coders[i]);
 }
 
 contenedor.innerHTML = frase;
